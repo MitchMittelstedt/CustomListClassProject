de31147391fff5430623a289f4e43842bb8269f7
Mitchell Mittelstedt <mmittelstedt@icloud.com>
2019-05-20 07:11:04 -0500

    added another test for zip testclass

--- CustomListClassProject/CustomListClassProjectUnitTesting/UnitTest1.cs
+++ CustomListClassProject/CustomListClassProjectUnitTesting/UnitTest1.cs
@@ -397,7 +397,7 @@ namespace CustomListClassProjectUnitTesting
             Assert.AreEqual(expected, actual);
         }
     }
-    
+
     [TestClass]
     public class Zip
     {
@@ -427,10 +427,35 @@ namespace CustomListClassProjectUnitTesting
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void Zip_Count_ReturnProperCountOfZippedList()
+        {
+            //ARRANGE
+            CustomList<int> listOne = new CustomList<int>();
+            CustomList<int> listTwo = new CustomList<int>();
+            int expected;
+            int actual;
+            //ACT
+            listOne.Add(0);
+            listOne.Add(1);
+            listOne.Add(2);
+            listOne.Add(3);
+            listTwo.Add(0);
+            listTwo.Add(1);
+            listTwo.Add(2);
+            listTwo.Add(3);
+            listOne.Zip(listTwo);
+
+            expected = 8;
+            actual = listOne.Count;
+
+            //ASSERT
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
-
-    
-
 
 
 
